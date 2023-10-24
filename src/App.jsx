6b98645b73b7bc17903a07880870857b92163965
Marcelo Lopes <marcelo.lopes2001@hotmail.com>
2023-10-24 18:59:21 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+function App() {
+  return (
+    <>
+       <header>
+        <section>
+        <div className="container">
+              <div className="even-columns">
+                <img className="logo" src="src\assets\white.png" alt="Produs Logo" />
+              </div>
+              <div className="even-columns | copy">
+                <h1>Para <span style={{color: "var(--clr-strong)"}}>estudante</span> que sonham grande</h1>
+                <img src="src\assets\sample.png" alt="Exemplo da Área de Membros" />
+                <p>Comece hoje a construir a vida que você sempre sonhou. Desenvolva as habilidades necessárias para ter sucesso nos estudos, ao lado de pessoas ambiciosas como você. A comunidade perfeita existe, e está só te esperando!</p>
+                <a href="">Quero fazer parte</a>
+              </div>
+            </div>
+        </section>
+
+        <section>
+          
+        </section>
+            
+        </header>
+        <main>
+          
+        </main>
+        <footer>
+
+        </footer>
+
+</>
+  )
+}
+
+export default App
