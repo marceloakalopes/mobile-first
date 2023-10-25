3eb39cd4bfec07753df7de61e7cce0e0949f4509
Marcelo Lopes <marcelo.lopes2001@hotmail.com>
2023-10-24 21:52:15 -0400

    Cards added

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,18 @@ function App() {
               <div className="even-columns">
                 <img className="logo" src="src\assets\white.png" alt="Produs Logo" />
               </div>
-              <div className="even-columns | copy">
-                <h1>Para <span style={{color: "var(--clr-strong)"}}>estudante</span> que sonham grande</h1>
+        </div>
+        </section>
+
+        <section>
+        <div className="container">
+          <div className="even-columns | copy">
+                <h1>Para <span className="clr-strong" >estudantes</span> que sonham grande</h1>
                 <img src="src\assets\sample.png" alt="Exemplo da Área de Membros" />
                 <p>Comece hoje a construir a vida que você sempre sonhou. Desenvolva as habilidades necessárias para ter sucesso nos estudos, ao lado de pessoas ambiciosas como você. A comunidade perfeita existe, e está só te esperando!</p>
                 <a href="">Quero fazer parte</a>
-              </div>
             </div>
+        </div>   
         </section>
 
         <section>
@@ -28,9 +33,32 @@ function App() {
         <section>
           <div className="container | valores">
             <div className="even-columns">
-              <h1>Educação. Inspiração. <span style={{color: "var(--clr-strong)"}}>Conexão</span></h1>
+              <h1>Educação. Inspiração. <span className="clr-strong">Conexão</span></h1>
               <p>A Produs Hub surgiu com o intuito de conectar, inspirar e munir de conhecimento os estudantes que buscam o alto desempenho. Juntos formaremos o futuro do Brasil.</p>
               <img src="src\assets\Aprender.png" alt="Valores da Produs" /> 
+            </div>
+            <div className="even-columns">
+
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div className="container | vantagens">
+            <div className="even-columns">
+              <h1>Mais que um simples clube de assinatura. <span className="clr-strong">Um estilo de vida</span>.</h1>
+            </div>
+            <div className="even-columns | card ">
+              <img src="src\assets\1-AGBrwMRLELI062R9.webp" alt="" />
+            </div>
+            <div className="even-columns | card">
+              <img src="src\assets\2-mnl7RnKZDMTNPGv3.webp" alt="" />
+            </div>
+            <div className="even-columns | card">
+              <img src="src\assets\3-YleLR2KD8eiy8kZQ.webp" alt="" />
+            </div>
+            <div className="even-columns | card">
+              <img src="src\assets\4-A0x4De6xV9Ca7MbG.webp" alt="" />
             </div>
           </div>
         </section>
